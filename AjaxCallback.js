c14e6c237e55382565af6db4756bc601b74c1baa
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-07-22 17:10:32 +0530

    added: view employee data from json server using ajax and promise

--- AjaxCallback.js
+++ AjaxCallback.js
@@ -4,53 +4,53 @@
 
 let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-function showTime() {
-    const date = new Date();
-    return date.getHours() + "Hrs:" + date.getMinutes() + "Mins:" + date.getSeconds() + "Secs:";
-}
-
-function makeAJAXCall(methoType, url, callback, async = true, data = null) {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        console.log("State Changed Called. Ready State: " + xhr.readyState + " Status" + xhr.status);
-        if (xhr.readyState === 4) {
-            //matching all 200 Series Responces
-            if (xhr.status === 200 || xhr.status === 201) {
-                callback(xhr.responseText);
-            } else if (xhr.status >= 400) {
-                console.log("Handle 400 Client Error or 500 Server Error")
+function makePromiseCall(methodType, url, async = true, data = null) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            console.log("State Changed Called. Ready State: " + xhr.readyState + " Status: " + xhr.status);
+            if (xhr.status.toString().match('^[2][0-9]{2}$')) {
+                resolve(xhr.responseText);
+            }else if(xhr.status.toString().match('^[4,5][0-9]{2}$')){
+                reject({
+                    status: xhr.status,
+                    statusText: xhr.statusText
+                });
+                console.log("XHR Failed");
             }
         }
-    }
 
-    xhr.open(methoType, url, async);
-    if (data) {
-        console.log(JSON.stringify(data));
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify(data));
-    } else xhr.send();
-    console.log(methoType + " request sent to the server at: " + showTime());
+        xhr.open(methodType, url, async);
+        if (data) {
+            console.log(JSON.stringify(data));
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(data));
+        } else xhr.send();
+        console.log(methodType + " request sent to the server");
+    });
 }
 
 const getURL = "http://127.0.0.1:3000/employee/";
-
-function getUserDetails(data) {
-    console.log("Get User Data at: " + showTime() + " data: " + data);
-}
-
-makeAJAXCall("GET", getURL, getUserDetails, true);
-console.log("Made GET AJAX Call to Server at: " + showTime());
+makePromiseCall("GET", getURL, true)
+    .then(responseText => {
+        console.log("Get User Data: " + responseText)
+    })
+    .catch(error => console.log("Get Error Status: " + JSON.stringify(error)));
 
 const deleteURL = "http://127.0.0.1:3000/employee/2";
-function userDeleted(data){
-    console.log("User Deleted at:" + showTime() + " data: " + data);
-}
-makeAJAXCall("DELETE", deleteURL, userDeleted, false);
+makePromiseCall("DELETE", deleteURL, false)
+    .then(responseText => {
+        console.log("Get User Data: " + responseText)
+    })
+    .catch(error => console.log("Get Error Status: " + JSON.stringify(error)));
 
 const postURL = "http://127.0.0.1:3000/employee";
-const emplData = {"name": "Harry","salary": "30000"};
-function userAdded(data){
-    console.log("User Added at:" + showTime() + " data: " + data);
-}
-makeAJAXCall("POST", postURL, userAdded, true, emplData);
-//curl Request:  curl -X GET -H "Content-Type: application/json" "http://127.0.0.1:3000/employee/" -w "\n"
+const emplData = {
+    "name": "Terish",
+    "salary": "30000"
+};
+makePromiseCall("POST", postURL, true, emplData)
+    .then(responseText => {
+        console.log("Get User Data: " + responseText)
+    })
+    .catch(error => console.log("Get Error Status: " + JSON.stringify(error)));
